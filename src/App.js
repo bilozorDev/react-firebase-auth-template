694cff3461927a5ab188293981f7c07fdb4dbc2b
bilazor4ik <bilazor@gmail.com>
2022-07-19 08:05:17 -0400

    basic layout and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
+import SharedLayout from "./components/SharedLayout";
+import AccountDetails from "./pages/AccountDetails";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 function App() {
   return (
-    <>
-    test
-    </>
+    <Routes>
+      <Route path="/" element={<SharedLayout/>}>
+      <Route index element={<Home/>}/>
+      <Route path="login" element={<Login/>}/>
+      <Route path="signup" element={<SignUp/>}/>
+      <Route path="account" element={<ProtectedRoute><AccountDetails/></ProtectedRoute>}/>
+      </Route>
+    </Routes>
   );
 }
 
